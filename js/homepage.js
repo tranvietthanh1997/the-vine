969141f9007bd9823cededa769ec1eeb0c538f98
Tran viet thanh <saoziha1997@gmail.com>
2020-07-20 02:27:54 +0700

    add responvie tab with navbar

--- js/homepage.js
+++ js/homepage.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  var isOpenSubmenu = true;
   $(".mobile-toggle-menu").click(function () {
     $(".mobile-nav").animate(
       {
@@ -14,6 +15,7 @@ $(document).ready(function () {
     );
     $(".overlay").css("visibility", "visible");
   });
+
   $(".close-mobile-nav").click(function () {
     $(".mobile-nav").animate(
       {
@@ -21,7 +23,38 @@ $(document).ready(function () {
       },
       500
     );
-
     $(".overlay").fadeTo("slow", 0);
   });
+
+  $.fn.animateRotate = function (angle, duration, easing, complete) {
+    var args = $.speed(duration, easing, complete);
+    var step = args.step;
+    return this.each(function (i, e) {
+      args.complete = $.proxy(args.complete, e);
+      args.step = function (now) {
+        $.style(e, "transform", "rotate(" + now + "deg)");
+        if (step) return step.apply(e, arguments);
+      };
+
+      $({ deg: 0 }).animate({ deg: angle }, args);
+    });
+  };
+
+  $(".mobile-nav-expand").click(function () {
+    $(".mobile-sub-menu").slideToggle("slow");
+
+    console.log(isOpenSubmenu);
+    if (isOpenSubmenu) {
+      $(".mobile-nav-expand").animateRotate(45);
+    } else {
+      $(".mobile-nav-expand").animateRotate(0);
+    }
+    isOpenSubmenu = !isOpenSubmenu;
+  });
+
+  if (isOpenSubmenu) {
+    $(".mobile-nav-expand").animateRotate(45);
+  } else {
+    $(".mobile-nav-expand").animateRotate(0);
+  }
 });
